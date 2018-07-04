fa2ffb3b825469b014c9317e2acdcd27c54260fb
tzepart <tzepart@gmail.com>
2018-07-05 01:59:57 +0300

    w_add_second_version_task_1

--- src/tasks/tasks_chapter_1/task_1/main.go
+++ src/tasks/tasks_chapter_1/task_1/main.go
@@ -17,11 +17,33 @@ func containsDuplicatesFirstVersion(checkingArray []string) bool {
 	return result
 }
 
+func containsDuplicatesSecondVersion(checkingArray []string) bool {
+
+	result := false
+	lengthArray := len(checkingArray)
+
+	for y:=0; y < lengthArray-1; y++ {
+		for x:=y+1; x < lengthArray; x++ {
+			if checkingArray[y] == checkingArray[x]{
+				result = true
+			}
+		}
+	}
+
+	return result
+}
+
 func main() {
-	checkingArray := []string{"one", "two", "one"}
+	checkingArray := []string{"one", "two", "one", "two"}
 	if containsDuplicatesFirstVersion(checkingArray){
-		fmt.Printf("Duplicates exist\n")
+		fmt.Printf("V1 - Duplicates exist\n")
 	}else{
-		fmt.Printf("Duplicates don't exist\n")
+		fmt.Printf("V1 - Duplicates don't exist\n")
+	}
+
+	if containsDuplicatesSecondVersion(checkingArray){
+		fmt.Printf("V2 - Duplicates exist\n")
+	}else{
+		fmt.Printf("V2 - Duplicates don't exist\n")
 	}
 }
