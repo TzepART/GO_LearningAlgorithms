d816aac15b575f2046a248d9ed888577c0654274
tzepart <tzepart@gmail.com>
2018-07-10 02:23:28 +0300

    w_add_methods-pointers-explained_go

---
+++ src/tutorial/methods/packages/methods-pointers-explained.go
@@ -0,0 +1,18 @@
+package methods
+
+import (
+	"math"
+)
+
+type Vertex5 struct {
+	X, Y float64
+}
+
+func Abs5(v Vertex5) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func Scale5(v *Vertex5, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
