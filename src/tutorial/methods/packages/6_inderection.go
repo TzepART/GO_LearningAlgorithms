0360a5746e28ba641b6529726e54ce5ee2bb5903
tzepart <tzepart@gmail.com>
2018-07-10 12:37:49 +0300

    w_add_6_inderection_go

---
+++ src/tutorial/methods/packages/6_inderection.go
@@ -0,0 +1,15 @@
+package methods
+
+type Vertex6 struct {
+	X, Y float64
+}
+
+func (v *Vertex6) Scale6(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func ScaleFunc6(v *Vertex6, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
